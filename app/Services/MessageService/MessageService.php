e849d8a9c7d88208d9ee57c6a520b3e3ba084610
suupoo <git@suu-poo.com>
2022-01-16 23:07:47 +0900

    個別チャットの投稿機能を作成

--- app/Services/MessageService/MessageService.php
+++ app/Services/MessageService/MessageService.php
@@ -2,9 +2,12 @@
 
 namespace App\Services\MessageService;
 
+use App\Models\MessageUser;
 use App\Repositories\MessageRepository;
 use App\Repositories\MessageUserRepository;
 use App\Repositories\PairRepository;
+use Illuminate\Support\Facades\DB;
+use mysql_xdevapi\Exception;
 
 class MessageService implements MessageServiceInterface
 {
@@ -46,6 +49,36 @@ class MessageService implements MessageServiceInterface
     {
         return
             $this->messageUserRepository->getMessageRoom($userId, $pairingUserId);
+    }
+
+    /**
+     * メッセージを送信する
+     *
+     * @param int $userId
+     * @param int $pairingUserId
+     * @param array $parameters
+     * @return mixed|null
+     */
+    public function sendMessage(int $userId, int $pairingUserId, array $parameters)
+    {
+        $messageUser = DB::transaction(function () use($userId, $pairingUserId, $parameters) {
+            // メッセージを先に登録する
+            $message = $this->messageRepository->create($parameters);
+
+            // メッセージと利用者の紐付けを行う
+            $messageUser = $this->messageUserRepository->create([
+                MessageUser::USER_ID    => $userId,
+                MessageUser::TO_USER_ID => $pairingUserId,
+                MessageUser::MESSAGE_ID => $message->id,
+            ]);
+
+            return $messageUser;
+        });
+
+        if($messageUser)
+            return $messageUser;
+
+        return null;
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::prefix('category')->name('category.')->group(function () {
 Route::prefix('message')->name('message.')->group(function () {
     Route::get('/', \App\Http\Controllers\MessageListController::class)->name('top');
     Route::get('/room/{pairing_user_id}', \App\Http\Controllers\MessageRoomController::class)->name('room');
+    Route::post('/room/{pairing_user_id}', \App\Http\Controllers\MessageRoomPostMessageController::class)->name('room-post-message');
 });
 
 
