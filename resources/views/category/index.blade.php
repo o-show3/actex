5ea8b3b983364a8e0b0727208e2a21dae9765615
suupoo <git@suu-poo.com>
2022-02-07 00:45:50 +0900

    ハッシュタグの画面デザインを修正

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -6,76 +6,129 @@
         興味のあるハッシュタグを選択しましょう！
     @endsection
     @section('content')
-    <div class="columns">
-        <div class="column">
-            <h1 class="title">登録済みハッシュタグ</h1>
-            <div class="field is-grouped is-grouped-multiline">
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">Technology</a>
-                        <a class="tag is-delete"></a>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">CSS</a>
-                        <a class="tag is-delete"></a>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">Flexbox</a>
-                        <a class="tag is-delete"></a>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">Web Design</a>
-                        <a class="tag is-delete"></a>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">Open Source</a>
-                        <a class="tag is-delete"></a>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">Community</a>
-                        <a class="tag is-delete"></a>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <div class="tags has-addons">
-                        <a class="tag is-link">Documentation</a>
-                        <a class="tag is-delete"></a>
-                    </div>
+    <div class="m-1">
+        <h1 class="display-6">新しいハッシュタグ</h1>
+        <p class="lead">新しいハッシュタグを設定できます</p>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    @for($i=0;$i<=34;$i++)
+                        <button type="button" class="btn btn-outline-primary">
+                            <i class="bi bi-hash"></i>
+                            Object
+                        </button>
+                    @endfor
                 </div>
             </div>
         </div>
     </div>
-    <div class="columns">
-        <div class="column"></div>
-    </div>
-    <h1 class="title">登録するハッシュタグ</h1>
-    <div class="columns">
-        @foreach($categories as $category)
-        <div class="column">
-            <form action="{{route('category.add', ['id' => $category->id])}}" method="post" >
-                @csrf
-                <input type="hidden" name="category_key" value="{{$category->uuid}}">
-                <input type="submit" value="{{$category->description}}" class="button is-primary is-light">
-            </form>
+    <div class="mt-4">
+        <h1 class="display-6">登録済みのハッシュタグ</h1>
+        <p class="lead">ハッシュタグを削除しましょう！</p>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    @for($i=0;$i<=34;$i++)
+                        <span class="badge rounded-pill bg-danger"><i class="bi bi-x"></i></span>
+                        <button class="btn btn-outline-primary" data-bs-toggle="modal"  data-bs-target="#staticBackdrop">
+                            <i class="bi bi-hash"></i>
+                            Object
+                        </button>
+                    @endfor
+                </div>
+            </div>
         </div>
-        @endforeach
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+            {{--    <div class="columns">--}}
+{{--        <div class="column">--}}
+{{--            <h1 class="title">登録済みハッシュタグ</h1>--}}
+{{--            <div class="field is-grouped is-grouped-multiline">--}}
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">Technology</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">CSS</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">Flexbox</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">Web Design</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">Open Source</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">Community</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div class="control">--}}
+{{--                    <div class="tags has-addons">--}}
+{{--                        <a class="tag is-link">Documentation</a>--}}
+{{--                        <a class="tag is-delete"></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="columns">--}}
+{{--        <div class="column"></div>--}}
+{{--    </div>--}}
+{{--    <h1 class="title">登録するハッシュタグ</h1>--}}
+{{--    <div class="columns">--}}
+{{--        @foreach($categories as $category)--}}
+{{--        <div class="column">--}}
+{{--            <form action="{{route('category.add', ['id' => $category->id])}}" method="post" >--}}
+{{--                @csrf--}}
+{{--                <input type="hidden" name="category_key" value="{{$category->uuid}}">--}}
+{{--                <input type="submit" value="{{$category->description}}" class="button is-primary is-light">--}}
+{{--            </form>--}}
+{{--        </div>--}}
+{{--        @endforeach--}}
+{{--    </div>--}}
     @endsection
 </x-layout>
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,16 +7,16 @@
     @endsection
     @section('content')
     <div class="m-1">
-        <h1 class="display-6">最近のトレンドタグ</h1>
-        <p class="lead">最近の熱い！トレンドタグ</p>
+        <h1 class="display-6">トレンドハッシュタグ</h1>
+        <p class="lead">最近トレンドなハッシュタグを見つけましょう！</p>
         <div class="container-fluid">
             <div class="row">
                 <div class="col">
                     @for($i=0;$i<=34;$i++)
-                    <button type="button" class="btn btn-outline-primary">
-                        <i class="bi bi-hash"></i>
-                        Hashtag
-                    </button>
+                        <button type="button" class="btn btn-outline-primary">
+                            <i class="bi bi-hash"></i>
+                            Hashtag
+                        </button>
                     @endfor
                 </div>
             </div>
@@ -28,52 +28,52 @@
         <div class="container-fluid">
             <div class="row">
                 @for($i=0;$i<=9;$i++)
-                <div class="col-4 mb-3">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">弊社所属タレント越岡裕貴（ふぉ〜ゆ〜）</h5>
-                            <p class="card-text">
-                                ジャニーズ事務所公式企業サイト。ごあいさつ、ロゴに込めた想い、会社概要、沿革、グループ会社一覧、お知らせなどジャニーズ事務所の企業情報を掲載しております。
-                            </p>
-                            <a href="#" target="_blank" class="btn btn-primary">開く</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            2022-01-31T11:53:49Z
+                    <div class="col-4 mb-3">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">弊社所属タレント越岡裕貴（ふぉ〜ゆ〜）</h5>
+                                <p class="card-text">
+                                    ジャニーズ事務所公式企業サイト。ごあいさつ、ロゴに込めた想い、会社概要、沿革、グループ会社一覧、お知らせなどジャニーズ事務所の企業情報を掲載しております。
+                                </p>
+                                <a href="#" target="_blank" class="btn btn-primary">開く</a>
+                            </div>
+                            <div class="card-footer text-muted">
+                                2022-01-31T11:53:49Z
+                            </div>
                         </div>
                     </div>
-                </div>
                 @endfor
             </div>
         </div>
     </div>
-{{--    <div class="columns">--}}
-{{--        <div class="column is-full">--}}
-{{--            <h1 class="title">最近の気になる話題</h1>--}}
-{{--            <h2 class="subtitle">最近のニュースをピックアップ！</h2>--}}
-{{--            <table class="table">--}}
-{{--                <thead>--}}
-{{--                    <tr>--}}
-{{--                        <th>公開日</th>--}}
-{{--                        <th>タイトル</th>--}}
-{{--                        <th>内容</th>--}}
-{{--                    </tr>--}}
-{{--                </thead>--}}
-{{--                <tbody>--}}
-{{--                    <tr>--}}
-{{--                        <td>2022-01-31T11:53:49Z</td>--}}
-{{--                        <td>弊社所属タレント越岡裕貴（ふぉ〜ゆ〜）・横原悠毅（IMPACTors） 新型コロナウイルス感染に関するご報告 | Johnny & Associates - ジャニーズ事務所</td>--}}
-{{--                        <td>ジャニーズ事務所公式企業サイト。ごあいさつ、ロゴに込めた想い、会社概要、沿革、グループ会社一覧、お知らせなどジャニーズ事務所の企業情報を掲載しております。</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td>2022-01-31T11:53:49Z</td>--}}
-{{--                        <td>1時間に約3回の電波放射を繰り返す「未知の天体」約4000光年先に発見 - sorae 宇宙へのポータルサイト</td>--}}
-{{--                        <td>カーティン大学／国際電波天文学研究センター（ICRAR）のNatasha Hurley-Walkerさんを筆頭とする研究グループは、強い電波放射を繰り返す未知の天体が天の川銀河の中で見つかったとする研</td>--}}
-{{--                    </tr>--}}
-{{--                </tbody>--}}
-{{--            </table>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    {{--    <div class="columns">--}}
+    {{--        <div class="column is-full">--}}
+    {{--            <h1 class="title">最近の気になる話題</h1>--}}
+    {{--            <h2 class="subtitle">最近のニュースをピックアップ！</h2>--}}
+    {{--            <table class="table">--}}
+    {{--                <thead>--}}
+    {{--                    <tr>--}}
+    {{--                        <th>公開日</th>--}}
+    {{--                        <th>タイトル</th>--}}
+    {{--                        <th>内容</th>--}}
+    {{--                    </tr>--}}
+    {{--                </thead>--}}
+    {{--                <tbody>--}}
+    {{--                    <tr>--}}
+    {{--                        <td>2022-01-31T11:53:49Z</td>--}}
+    {{--                        <td>弊社所属タレント越岡裕貴（ふぉ〜ゆ〜）・横原悠毅（IMPACTors） 新型コロナウイルス感染に関するご報告 | Johnny & Associates - ジャニーズ事務所</td>--}}
+    {{--                        <td>ジャニーズ事務所公式企業サイト。ごあいさつ、ロゴに込めた想い、会社概要、沿革、グループ会社一覧、お知らせなどジャニーズ事務所の企業情報を掲載しております。</td>--}}
+    {{--                    </tr>--}}
+    {{--                    <tr>--}}
+    {{--                        <td>2022-01-31T11:53:49Z</td>--}}
+    {{--                        <td>1時間に約3回の電波放射を繰り返す「未知の天体」約4000光年先に発見 - sorae 宇宙へのポータルサイト</td>--}}
+    {{--                        <td>カーティン大学／国際電波天文学研究センター（ICRAR）のNatasha Hurley-Walkerさんを筆頭とする研究グループは、強い電波放射を繰り返す未知の天体が天の川銀河の中で見つかったとする研</td>--}}
+    {{--                    </tr>--}}
+    {{--                </tbody>--}}
+    {{--            </table>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
     @endsection
 </x-layout>
 
